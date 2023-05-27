3eaa9ce508dbd47b9729e53ba33de74f65fe9134
Aryan Singh <aryan7109singh@gmail.com>
2023-05-28 01:56:02 +0530

    Update ParamRoomSim.py

--- basic_vPython/ParamRoomSim.py
+++ basic_vPython/ParamRoomSim.py
@@ -18,12 +18,14 @@ ylen = 10
 zlen = 10
 thickness = .1
 
-walls = {(xlen, thickness, ylen):((0, -ylen/2, 0), (0, ylen/2, 0)), (thickness, ylen, zlen):((-xlen/2, 0, 0),(xlen/2, 0, 0)), (xlen, ylen, thickness):((0,0,-zlen/2),(0,0,zlen/2))}
+walls = [((xlen, thickness, zlen), (0, -ylen/2, 0), (0, ylen/2, 0)), ((thickness, ylen, zlen), (-xlen/2, 0, 0), (xlen/2, 0, 0)), ((xlen, ylen, thickness), (0,0,-zlen/2), (0,0,zlen/2))]
 
-for w in walls.keys():
-    for i in range(2):
-        boxTemp = box(size=vector(w[0],w[1],w[2]), pos=vector(walls[w][i][0], walls[w][i][1], walls[w][i][2]), color=vector(random.randrange(0, 10)/10, random.randrange(0, 10)/10, random.randrange(0, 10)/10))
-    
+for i in [0,1,2]:
+    for j in [1,2]:
+        print(walls[i][0][0],walls[i][0][1],walls[i][0][2])
+        print(walls[i][j][0], walls[i][j][1], walls[i][j][2])
+        boxTemp = box(size=vector(walls[i][0][0],walls[i][0][1],walls[i][0][2]), pos=vector(walls[i][j][0], walls[i][j][1], walls[i][j][2]), color=vector(random.randrange(0, 10)/10, random.randrange(0, 10)/10, random.randrange(0, 10)/10))
+
 while True:
     pass
 
