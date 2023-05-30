081eaed5f7e861093c638e5a5b3d2d3e40a63f4a
Aryan Singh <aryan7109singh@gmail.com>
2023-05-30 22:43:15 +0530

    Add myPiston.py

---
+++ basic_vPython/myPiston.py
@@ -0,0 +1,60 @@
+#   vPython Orientation:
+#     y
+#     |
+#     |  laptop
+#     |  screen
+#     |_ _ _ _ _ x 
+#    /
+#   / 
+#  /  
+# z (out of the screen)
+
+from vpython import *
+import time
+import random
+
+piston1 = cylinder(radius=1, length=0, color=color.red, opacity=1)
+delta = 0.01
+
+def animateLength(shape, delta, r, lower, upper, dir):
+    if dir == 'up':
+        while shape.length <= upper:
+            rate(r)
+            shape.length += delta
+
+    elif dir == 'down':
+        while shape.length > lower + delta:
+            rate(r)
+            shape.length -= delta
+
+def animateOpacity(shape, delta, r, lower, upper, dir):
+    if dir == 'up':
+        while shape.opacity <= upper:
+            rate(r)
+            shape.opacity += delta
+
+    elif dir == 'down':
+        while shape.opacity > lower + delta:
+            rate(r)
+            shape.opacity -= delta
+
+def animateRadius(shape, delta, r, lower, upper, dir):
+    if dir == 'up':
+        while shape.radius <= upper:
+            rate(r)
+            shape.radius += delta
+
+    elif dir == 'down':
+        while shape.radius > lower + delta:
+            rate(r)
+            shape.radius -= delta
+
+while True:
+    animateLength(piston1, delta, 60, 0, 2, 'up')
+    animateOpacity(piston1, delta, 60, 0, 1, 'down')
+    animateOpacity(piston1, delta, 60, 0, 1, 'up')
+    animateLength(piston1, delta, 60, 0, 2, 'down')
+    animateLength(piston1, delta, 60, 0, 2, 'up')
+    animateRadius(piston1, delta, 60, 0, 1, 'down')
+    animateRadius(piston1, delta, 60, 0, 1, 'up')
+    animateLength(piston1, delta, 60, 0, 2, 'down')
